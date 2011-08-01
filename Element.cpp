4637b1cd3273ae2537e2231d65eb083c41dc937c
parivana <parivana@gmail.com>
2011-08-01 16:59:53 +0900

    Element 클래스 변경

--- Element.cpp
+++ Element.cpp
@@ -9,7 +9,35 @@ Element::Element(const char *name, UIntRect rect)
 	this->rect = rect;
 }
 
+Element::~Element()
+{
+	if(this->param)
+		delete this->param;
+}
+
 void Element::setName(const char *name) 
 {
 	strncpy(this->name, name, ENAME_SIZE);
+}
+
+void Element::setParam(const char *param)
+{
+	int length = strlent(param);
+	if(this->param)
+		delete this->param;
+
+	this->param = new char[length];
+	strncpy(this->param, param, length);
+}
+
+void Element::setRect(
+		unsigned int left,
+		unsigned int top,
+		unsigned int right,
+		unsigned int bottom) 
+{
+	this->rect.left = left;
+	this->rect.top = top;
+	this->rect.right = right;
+	this->rect.bottom = bottom;
 }
--- Element.h
+++ Element.h
@@ -10,13 +10,23 @@ class Element {
 		Element() {};
 		Element(const char *name, UIntRect rect);
 
-		virtual ~Element() {};
+		virtual ~Element();
 
 		void setName(const char *name);
 		char *getName() { return name; };
 
+		void setParam(const char *name);
+		char *getParam() { return param; };
+
+		void setRect(unsigned int left, unsigned int top,
+				unsigned int right, unsigned int bottom);
+		//void setRect(UIntRect r) { this->rect = r; };
+		UIntRect getRect() { return rect; };
+
 	private:
 		char name[ENAME_SIZE];
+		char *param;
+		int type;
 		UIntRect rect;
 };
 
