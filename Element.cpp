6f05ad38a89f50f6c2e9b0e8abd936379f7cec77
parivana <parivana@gmail.com>
2011-08-01 16:59:53 +0900

    Element 클래스 변경

--- Element.cpp
+++ Element.cpp
@@ -9,7 +9,72 @@ Element::Element(const char *name, UIntRect rect)
 	this->rect = rect;
 }
 
+Element::~Element()
+{
+	if(this->param)
+		delete this->param;
+
+	if(this->wire)
+		delete this->wire;
+}
+
 void Element::setName(const char *name) 
 {
 	strncpy(this->name, name, ENAME_SIZE);
+}
+
+void Element::setParam(const char *param)
+{
+	int length = strlent(param);
+	if(this->param)
+		delete this->param;
+
+	this->param = new char[length];
+	strncpy(this->param, param, length);
+}
+
+void Element::setRect(
+		unsigned int left,
+		unsigned int top,
+		unsigned int right,
+		unsigned int bottom) 
+{
+	this->rect.left = left;
+	this->rect.top = top;
+	this->rect.right = right;
+	this->rect.bottom = bottom;
+}
+
+void Element::setNport(int nport)
+{
+	this->nport = nport;
+}
+
+int Element::makeWire()
+{
+	if(this->nport <= 0)
+		return -1;
+
+	this->wire = new (Wire *)[this->nport];
+	return 0;
+}
+
+int Element::setWire(Wire *w, int port)
+{
+	if(!this->wire)
+		return -1;
+
+	this->wire[port] = w;
+	return 0;
+}
+
+Wire *Element::getWire(int port) 
+{
+	if(!this->wire)
+		return NULL;
+
+	if(port >= this->nport)
+		return NULL;
+
+	return this->wire[port];
 }
--- Element.h
+++ Element.h
@@ -2,6 +2,7 @@
 #define __ELEMENT_H__
 
 #include "Rect.h"
+#include "Wire.h"
 
 #define ENAME_SIZE 8
 
@@ -10,14 +11,33 @@ class Element {
 		Element() {};
 		Element(const char *name, UIntRect rect);
 
-		virtual ~Element() {};
+		virtual ~Element();
 
 		void setName(const char *name);
 		char *getName() { return name; };
 
+		void setParam(const char *name);
+		char *getParam() { return param; };
+
+		void setRect(unsigned int left, unsigned int top,
+				unsigned int right, unsigned int bottom);
+		//void setRect(UIntRect r) { this->rect = r; };
+		UIntRect getRect() { return rect; };
+
+		void setNport(int nport);
+		int getNport() { return nport; };
+
+		void makeWire();
+		int setWire(Wire *w, int port);
+		Wire *getWire(int port);  
+
 	private:
 		char name[ENAME_SIZE];
+		char *param;
+		int type;
+		int nport;
 		UIntRect rect;
+		Wire **wire;
 };
 
 #endif // __ELEMENT_H__
